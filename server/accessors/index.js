cf1cf321b14b96e6ea2e72f6803a3df25ba4ac1c
Luc Gauthier <lucgauthier@gmail.com>
2023-02-18 09:06:35 -0500

    remove openweather integration

--- server/accessors/index.js
+++ server/accessors/index.js
@@ -1,7 +1,6 @@
 module.exports = {
     calendarAccessor: require('./calendar'),
     icalAccessor: require('./ical'),
-    openweatherAccessor: require('./openweather'),
     pirateweatherAccessor: require('./pirateweather'),
     templateAccessor: require('./template/template'),
 }
